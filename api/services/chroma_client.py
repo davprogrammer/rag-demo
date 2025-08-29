d907fdac09530e55ffc77db70f4dd1ef15d00757
David Wiese <david.wiese04@gmail.com>
2025-08-29 11:48:02 +0200

    bug fix

--- api/services/chroma_client.py
+++ api/services/chroma_client.py
@@ -1,14 +1,11 @@
-# api/services/chroma_client.py
 import chromadb
 from . import config
 
 def get_client():
-    # tenant/database hier direkt an HttpClient, NICHT in Settings
+    
     return chromadb.HttpClient(
         host=config.CHROMA_HOST,
         port=config.CHROMA_PORT,
-        tenant=config.TENANT,
-        database=config.DATABASE,
     )
 
 def get_collection():
