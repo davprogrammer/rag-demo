c7fd346de62844941bbff4e6a8eb1f8e78053d3c
David Wiese <david.wiese04@gmail.com>
2025-09-09 11:38:28 +0200

    test

--- api/services/routes.py
+++ api/services/routes.py
@@ -63,8 +63,8 @@ def chat_completions(payload: dict = Body(...),authorization: str | None = Heade
     
     messages = payload.get("messages", [])
     user_msg = ""
-   
-    logging.info(f"[User] {m.get("role")}")
+    user = m.get("role")
+    logging.info(f"[User] {user}")
     
     for m in reversed(messages):
         if m.get("role") == "user":
